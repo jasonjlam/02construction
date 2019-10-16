ad8fc4cf339f9119ddb9442a00cc7f9a3ab01258
Jason <jlam01@stuy.edu>
2019-10-16 19:55:56 -0400

    Added debugging prints

--- construction_main.c
+++ construction_main.c
@@ -5,16 +5,21 @@
 
 int main() {
 	srand (time(NULL));
+	printf("Randomly print five pokemon \n");
 	int i = 0;
+
 	for (i; i < 5; i ++) {
 		pokePrint(pokeSelect());
 	}
+	printf("Print a Zorua \n");
 	struct pokemon zorua;
 	zorua.name = "Zorua";
 	zorua.type = "dark";
 	zorua.level = 5;
 	pokePrint(zorua);
+	printf("Zorua is evolving... \n");
 	pokeModify(&zorua, "Zoroark", "dark", 30);
+	printf ("Modified Zorua (Zoroark) \n");
 	pokePrint(zorua);
 	return 0;
 }
